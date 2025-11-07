70fecd392ae041c648e5475bc00a5cab62678baf
webbercobb <146506759+webbercobb@users.noreply.github.com>
2025-11-06 22:54:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Toggle overlay when clicking images
+document.querySelectorAll('.work-item').forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
